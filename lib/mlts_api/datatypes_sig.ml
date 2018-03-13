5a7e3b61df7531ed7e7395e383e1d15824763822
VooDooS <thevoodoos@gmail.com>
2018-03-13 16:02:39 +0100

    Debugging arrow types

--- lib/mlts_api/datatypes_sig.ml
+++ lib/mlts_api/datatypes_sig.ml
@@ -1,25 +1,27 @@
 open Datatypes
 
 let arity_type_of_atypl =
-  let rec aux (ty, i) =
+  let rec aux c (ty, i) =
     match ty with
     | Consl(_) -> "tm"
     | Bindl(at1, at2)
-      -> let r1, r2 = aux at1, aux at2 in
+      -> let r1, r2 = aux c at1, aux c at2 in
          "(" ^ r1 ^ " -> " ^ r2 ^ ")"         
     | Listl(at1)
-      -> let r1 = aux at1 in
+      -> let r1 = aux c at1 in
          r1
     | Arrowl(at1, at2)
-      -> let r1, r2 = aux at1, aux at2 in
-         r1 ^ " -> " ^ r2
+      -> let r1, r2 = aux (c+1) at1, aux (c+1) at2 in
+         if c = 0 then
+           r1 ^ " -> " ^ r2
+         else r1
     | Suml(l) -> if i > 0 then
                    LpStrings.to_separated_list ~nop:true
                      " -> "
-                     (List.map (aux) l)
+                     (List.map (aux c) l)
                  else "tm"
   in
-  aux
+  aux 0
     
 let gen_sig cname atypl =
   let aux_val name =
