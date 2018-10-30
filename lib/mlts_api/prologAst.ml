af92c0804f9722f2acd6e8bf3015be6cd6f53c65
VooDooS <thevoodoos@gmail.com>
2018-10-30 11:52:20 +0100

    Ajout Prolog ast

---
+++ lib/mlts_api/prologAst.ml
@@ -0,0 +1,36 @@
+type global_name = string
+type local_name = string * int
+type name = 
+| Local of local_name
+| Global of global_name
+
+type ty = 
+| Name of global_name
+| Arrow of ty * ty
+
+type term = 
+| Abs of local_name * term
+| Hyp of term * term
+| Eq of term * term
+| App of atom * term list
+| Seq of term list
+and atom = name
+
+type decl = { 
+    sort: decl_sort; 
+    name: global_name; 
+    ty: ty; 
+}
+and decl_sort = Kind | Type
+
+type def = {
+    name: global_name;
+    args: term list;
+    body: term;
+}
+
+type clause =
+| Declaration of decl
+| Definition of def
+
+type prog = clause list
